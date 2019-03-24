69627b50c38096f9c77a0b21f5780a555fa5eed9
kw585 <kw585@cornell.edu>
2019-03-24 00:47:46 -0400

    force server port to 5000

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import openSocket from 'socket.io-client';
+// import openSocket from 'socket.io-client';
+import io from '../../socket.io';
 import { unregister } from "./serviceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 require('./styles/style.scss');
 
-const socket = openSocket('https://immense-anchorage-61325.herokuapp.com:5000');
+// const socket = openSocket('https://immense-anchorage-61325.herokuapp.com:5000');
+const socket = io();
+console.log(socket);
 console.log("here!");
 
 ReactDOM.render(<App socket={socket}/>, document.getElementById('root'));
