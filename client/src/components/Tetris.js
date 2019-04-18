60892d0010a0dc225a2a5c1d32ed777fe1b13fa8
cedarforest7 <jacquelineyin7@gmail.com>
2019-04-18 18:17:15 -0400

    add popup

--- client/src/components/Tetris.js
+++ client/src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import GamePanel from './GamePanel';
-//import GameInfo from './GameInfo';
+import './Tetris.css'
 import styled from 'styled-components';
 
 const Left = styled.div`
@@ -20,21 +20,61 @@ padding-top: 10px;
 class Tetris extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            gameOver: false,
+            showPopup: false
+        }
+
+    }
+
+    componentDidMount() {    
+        this.gameStatus();
+    }
+
+    gameStatus() {
+        this.props.socket.on('game_over',() => {
+            this.setState({gameOver: true, showPopup: true});
+        });
     }
 
     render() {
+
         return (
             <div>
                 <Left>
                     <GamePanel socket={this.props.socket}/>
                 </Left>
-                {/* <Right>
-                    <GameInfo />
-                </Right> */}
+                {this.state.showPopup ? 
+                        <Popup
+                            text='Take a survey?'
+                            closePopup={this.togglePopup.bind(this)}
+                        />
+                    : null
+                }
             </div>
         )
     }
+    togglePopup() {
+        this.setState({
+          showPopup: !this.state.showPopup
+        });
+    }
 }
+
+class Popup extends React.Component {
+    render() {
+      return (
+        <div className='popup'>
+            <div className='popup_inner'>
+                <h1>{this.props.text}</h1>
+                <button onClick={this.props.closePopup}>Yes</button>
+                <button onClick={this.props.closePopup}>No</button>
+            </div>
+        </div>
+        
+      );
+    }
+  }
 
 
 export default Tetris;
