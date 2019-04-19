9401c4a437e42ca9a85641bfa667aa6dfa7cb5e7
Liuting <chenliuting95@gmail.com>
2019-04-18 21:53:48 -0400

    add redirect  button in popup after game

--- client/src/components/Tetris.js
+++ client/src/components/Tetris.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import GamePanel from './GamePanel';
 import './Tetris.css'
 import styled from 'styled-components';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
+
+
+
 
 const Left = styled.div`
 display: inline-block;
@@ -22,18 +27,28 @@ class Tetris extends Component {
         super(props);
         this.state = {
             gameOver: false,
-            showPopup: false
+            showPopup: false,
+            score : 0,
+            redirectSurvey: false,
+            redirectHome: false
         }
 
     }
 
     componentDidMount() {    
         this.gameStatus();
+        this.updateScore();
     }
 
     gameStatus() {
         this.props.socket.on('game_over',() => {
             this.setState({gameOver: true, showPopup: true});
+        });
+    }
+
+    updateScore() {
+        this.props.socket.on('score', (data) => {
+            this.setState({score: data.score});
         });
     }
 
@@ -46,6 +61,7 @@ class Tetris extends Component {
                 </Left>
                 {this.state.showPopup ? 
                         <Popup
+                            finalscore = {this.state.score}
                             text='Take a survey?'
                             closePopup={this.togglePopup.bind(this)}
                         />
@@ -59,16 +75,26 @@ class Tetris extends Component {
           showPopup: !this.state.showPopup
         });
     }
+    
 }
 
 class Popup extends React.Component {
+    // toSurvey = () => {
+    //     this.props.history.push(`/survey`)
+    // };
+    // toHome = () => {
+    //     this.props.history.push(`/home`)
+    // };
+
     render() {
       return (
         <div className='popup'>
             <div className='popup_inner'>
-                <h1>{this.props.text}</h1>
-                <button onClick={this.props.closePopup}>Yes</button>
-                <button onClick={this.props.closePopup}>No</button>
+                <h1>Score:<br /> {this.props.finalscore}<br />{this.props.text}</h1>
+                <div className='button'>
+                <NavLink to="/survey">Yes</NavLink>
+                <NavLink to="/home">No</NavLink>
+                </div>
             </div>
         </div>
         
